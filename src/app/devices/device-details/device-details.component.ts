883e20bfbbff4e0d47f2bf82872c1ef8fb03a7cb
Harshal Shah <harshal.shah25@t-mobile.com>
2018-10-07 19:58:43 +0530

    email notifications

--- src/app/devices/device-details/device-details.component.ts
+++ src/app/devices/device-details/device-details.component.ts
@@ -1,7 +1,6 @@
-
+import { Component, OnInit, ViewContainerRef, ViewChild  } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
-import { Component, OnInit, ViewContainerRef, ViewChild  } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
@@ -17,15 +16,54 @@ import { OlMapComponent } from './../../shared/ol-map/ol-map.component';
   styleUrls: ['./device-details.component.css']
 })
 export class DeviceDetailsComponent implements OnInit {
-  deviceId : string;
-  deviceDetails = {};
-  constructor(  private route: ActivatedRoute) { 
-  
-  }
+  currentUserDetails = JSON.parse(JSON.stringify(ParentChildCommService.currentUserDetails));
+  deviceDetail = { lat : 0, lng:0, manufacturer:'', model:''};
+  markerData = [];
+  deviceId: string;
+  constructor(private http: HttpClient,private spinner: NgxSpinnerService,private toastr: ToastrService,
+    private router: Router, private route: ActivatedRoute) {
+   
+ }
+ @ViewChild(OlMapComponent) olMap:OlMapComponent;
   ngOnInit() {
     this.deviceId = this.route.snapshot.paramMap.get('id');
-      // this.deviceDetails = ParentChildCommService.devices.filter(
-      //   device => device.deviceId === this.deviceId);
+    this.getdeviceDetails();
   }
 
+  getdeviceDetails() {
+    var url = global.BASE_API_URL + "device/device_details";
+    var params = { uid : this.currentUserDetails.user.uid , device_unique_id: this.deviceId };
+    this.spinner.show();
+    this.http.post(url, params)
+    .pipe(
+      catchError((err, caught) => {
+        this.spinner.hide();
+        this.toastr.error('Something bad happened; please try again later.');
+        return throwError(
+          'Something bad happened; please try again later.');
+      })
+    )
+    .subscribe((data: Response) => {
+      this.spinner.hide();
+      var response = JSON.parse(JSON.stringify(data));   
+      this.deviceDetail = response; 
+
+      let marker = {
+        lat : this.deviceDetail.lat,
+        lng : this.deviceDetail.lng,
+        description : "<b>Manufacturer </b>" + this.deviceDetail.manufacturer + "<br><b>Model </b>" + this.deviceDetail.model
+      };
+
+      this.markerData.push(marker);
+
+    var area = {
+      lat:20.5937,
+      lng:78.9629,
+      zoomLevel:6
+    }
+
+    this.olMap.renderMapMultipleMarkers(this.markerData,area);
+      
+    });
+  }
 }
--- src/app/settings/email-notifications/email-notifications.component.ts
+++ src/app/settings/email-notifications/email-notifications.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import * as global from '../../shared/global'
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { ParentChildCommService } from './../../shared/ParentChildCommService';
 
 @Component({
   selector: 'app-email-notifications',
@@ -7,9 +15,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailNotificationsComponent implements OnInit {
 
-  constructor() { }
+  currentUserDetails = JSON.parse(JSON.stringify(ParentChildCommService.currentUserDetails));
+
+  IsEnteredGeofence = { checked : false, value : 1 };
+  IsExitedGeofence  = { checked : false, value : 2 };
+  IsDeviceBoot  = { checked : false, value : 4 };
+  IsDeviceShutdown  = { checked : true, value : 8 };
+  IsSIMChanged  = { checked : false, value : 16 };
+  IsDeviceNotSeen   = { checked : false, value : 32 };
+
+  preferences : number;
+
+  constructor(private http: HttpClient, private spinner: NgxSpinnerService, private toastr: ToastrService,
+    private router: Router) {
+
+  }
 
   ngOnInit() {
+  }
+
+  savePreferences(){
+    this.getPreferences();
+    var url = global.BASE_API_URL + "user/update_user";
+    var params = { uid : this.currentUserDetails.user.uid, notifications: this.preferences };
+    this.spinner.show();
+    this.http.post(url, params)
+      .pipe(
+        catchError((err, caught) => {
+          this.spinner.hide();
+          this.toastr.error(err.error);
+          return throwError(
+            'Something bad happened; please try again later.');
+        })
+      )
+      .subscribe((data: Response) => {
+        var response = JSON.parse(JSON.stringify(data));
+
+        this.spinner.hide();
+
+        if (response.result == "success") {
+          this.toastr.success(response.message);
+        }
+        else if (response.result == "fail") {
+          this.toastr.error(response.message);
+        }
+        else {
+          this.toastr.error('Something bad happened; please try again later.');
+        }
+
+      });
+  }
+
+  getPreferences(){
+    this.preferences = 0;
+    if(this.IsEnteredGeofence && this.IsEnteredGeofence.checked)
+    {
+      this.preferences += this.IsEnteredGeofence.value;
+    }
+    if(this.IsExitedGeofence && this.IsExitedGeofence.checked)
+    {
+      this.preferences += this.IsExitedGeofence.value;
+    }
+    if(this.IsDeviceBoot && this.IsDeviceBoot.checked)
+    {
+      this.preferences += this.IsDeviceBoot.value;
+    }
+    if(this.IsDeviceShutdown && this.IsDeviceShutdown.checked)
+    {
+      this.preferences += this.IsDeviceShutdown.value;
+    }
+    if(this.IsSIMChanged && this.IsSIMChanged.checked)
+    {
+      this.preferences += this.IsSIMChanged.value;
+    }
+    if(this.IsDeviceNotSeen && this.IsDeviceNotSeen.checked)
+    {
+      this.preferences += this.IsDeviceNotSeen.value;
+    }
+  }
+
+  setPreferences(){
+    
   }
 
 }
